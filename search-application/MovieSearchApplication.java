493918d2aab3ff9b3e41874fb7ffc598445cfa65
Janani <2317022@nec.edu.in>
2025-10-28 11:12:58 +0530

    Create MovieSearchApplication.java

---
+++ search-application/MovieSearchApplication.java
@@ -0,0 +1,9 @@
+public class MovieSearchApplication {
+		public static void main(String[] args) {
+			System.out.println("Guardians of the Galaxy Vol. 2");
+			System.out.println("Response from OMDB API:");
+			System.out.println("https://www.omdbapi.com/?i=tt3896198&apikey=35b23593");
+			System.out.println("Movie Found Successfully");
+			}
+	}
+		
